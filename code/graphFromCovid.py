9146228db29fe0b8380cd1fc80411e0b89e98de1
mayababu <mayababu73@gmail.com>
2020-03-29 15:03:14 +1300

    Added the number of recoveries, and added colour to graph, and type casted the y axis to ints

--- code/graphFromCovid.py
+++ code/graphFromCovid.py
@@ -1,10 +1,13 @@
 import csv 
 import requests
+import urllib.request
 import matplotlib.pyplot as plt
 
 url = "https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv&filename=time_series_covid19_confirmed_global.csv"
+urlRecovery = "https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_recovered_global.csv&filename=time_series_covid19_recovered_global.csv"
 
 try:
+    #reading the confirmed cases file
     with requests.Session() as S:
         download = S.get(url)
         
@@ -18,9 +21,30 @@ try:
             xAxis = row[4:]
 
         if row[1] == 'New Zealand':
-            yAxis = row[4:]
-    
-    plt.plot(xAxis,yAxis, label = "From John Hopkins")
+            yAxisString = row[4:]
+            confirmedYInt =[] 
+            for numConfirmedCases in yAxisString:
+                confirmedYInt.append(int(numConfirmedCases))
+
+    #reading the recovery file
+    downloadRecoveryOpen = urllib.request.urlopen(urlRecovery)
+    decodedRecoveryDownload = downloadRecoveryOpen.read()
+    decodedRecoveryDownloadDecoded = decodedRecoveryDownload.decode('utf-8')
+
+    readRecoveryCsv = csv.reader(decodedRecoveryDownloadDecoded.splitlines(), delimiter = ',')
+    recoveryCsvList = list(readRecoveryCsv)
+
+    for row in recoveryCsvList:
+        if row[1] == 'New Zealand':
+            recoveryYAxisString = row[4:]
+            recoveryYAxisInt = []
+            for numCases in recoveryYAxisString:
+                recoveryYAxisInt.append(int(numCases))
+
+
+    plt.plot(xAxis,confirmedYInt, color = 'red', label = "Confirmed")
+    plt.plot(xAxis,recoveryYAxisInt, color='purple' ,label = "Recovered")
+    plt.legend(loc="upper left")
     plt.xlabel('Date')
     plt.ylabel('Number of Cases')
     plt.title('Covid19 New Zealand Cases')
