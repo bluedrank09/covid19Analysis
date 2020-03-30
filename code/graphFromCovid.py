bf47f73d5043ebbfbfdb9347fa994159a382114e
mayababu <mayababu73@gmail.com>
2020-03-30 14:42:44 +1300

    Added the number of deaths, changed the colour of the confirmed cases line, replaced requests with urllibs

--- code/graphFromCovid.py
+++ code/graphFromCovid.py
@@ -1,19 +1,18 @@
 import csv 
-import requests
 import urllib.request
 import matplotlib.pyplot as plt
 
 url = "https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv&filename=time_series_covid19_confirmed_global.csv"
 urlRecovery = "https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_recovered_global.csv&filename=time_series_covid19_recovered_global.csv"
+urlDeaths = "https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_deaths_global.csv&filename=time_series_covid19_deaths_global.csv"
 
 try:
     #reading the confirmed cases file
-    with requests.Session() as S:
-        download = S.get(url)
-        
-    decodedDownload = download.content.decode('utf-8')
+    downloadOpen = urllib.request.urlopen(url)
+    decodedDownload = downloadOpen.read()
+    decodedDownloadDecoded = decodedDownload.decode('utf-8')
 
-    readCsv = csv.reader(decodedDownload.splitlines(),delimiter=',')
+    readCsv = csv.reader(decodedDownloadDecoded.splitlines(),delimiter=',')
     csvList = list(readCsv)
 
     for row in csvList:
@@ -41,9 +40,26 @@ try:
             for numCases in recoveryYAxisString:
                 recoveryYAxisInt.append(int(numCases))
 
+    #reading the deaths file 
+    downloadDeathsOpen = urllib.request.urlopen(urlDeaths)
+    decodedDeathsDownload = downloadDeathsOpen.read()
+    decodedDeathsDownloadDecoded = decodedDeathsDownload.decode('utf-8')
 
-    plt.plot(xAxis,confirmedYInt, color = 'red', label = "Confirmed")
+    readDeathsCsv = csv.reader(decodedDeathsDownloadDecoded.splitlines(),delimiter=',')
+    deathsCsvList = list(readDeathsCsv)
+
+    for row in deathsCsvList:
+        if row[1] == 'New Zealand':
+            deathsYAxisString = row[4:]
+            deathsYAxisInt = []
+            for numDeathsCases in deathsYAxisString:
+                deathsYAxisInt.append(int(numDeathsCases))
+
+
+
+    plt.plot(xAxis,confirmedYInt, color = 'green', label = "Confirmed")
     plt.plot(xAxis,recoveryYAxisInt, color='purple' ,label = "Recovered")
+    plt.plot(xAxis,deathsYAxisInt, color = 'red', label = "Deaths")
     plt.legend(loc="upper left")
     plt.xlabel('Date')
     plt.ylabel('Number of Cases')
